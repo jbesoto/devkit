9ab0095597cc47a339efbf0d61d75d9f7fa46f0a
Juan Diego Becerra <jdb9056@nyu.edu>
2024-09-20 13:14:48 -0500

    feature: Support Template Selection with fzf
    Fuzzy-finder is used to interactively select a template from the
    `templates/` directory.
    
    A template consists of:
    
      - A folder named with the template's name
      - A `structure.yaml` used to represent the desired structure of the
        repository
      - An optional `blueprints/` directory containing files listed in the
        structure with specific contents to be applied
    
    Note that the structure file must exist, otherwise the program will
    exit.

--- crepo/crepo.sh
+++ crepo/crepo.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
-TEMPLATES_DIR='~/.dotfiles/crepo/templates'
-TARGET=$@
+set -euo pipefail
 
-cd ${TARGET}
-git init .
+readonly TEMPLATES_DIR="${HOME}/.dotfiles/crepo/templates"
+readonly TARGET_DIR="$1"
 
-# List templates
-# Select template
-# Parse `struct.yaml`
-# Replace blueprints
+main() {
+  if [[ ! -d "${TARGET_DIR}" ]]; then
+    mkdir -p "${TARGET_DIR}"
+  fi
+
+  if [[ ! -d ".git" ]]; then
+    git init "${TARGET_DIR}"
+  fi
+
+  template="$(select_template)"
+  readonly template
+}
+
+select_template() {
+  local template
+
+  template="$(find "${TEMPLATES_DIR}" -mindepth 1 -maxdepth 1 -type d \
+    | awk -F/ '{print $NF "\t" $0}' \
+    | fzf --with-nth 1 --prompt "Template: " \
+    | cut -f2)"
+
+  if [[ -z "${template}" ]]; then
+    echo "crepo: Template not selected."
+    exit 1
+  fi
+
+  if [[ ! -e "${template}/structure.yaml" ]]; then
+    echo "crepo: Repository structure not found."
+    exit 1
+  fi
+
+  echo "${template}"
+}
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: crepo <TARGET>"
+  exit 1
+fi
+
+main "${TARGET_DIR}"
